1c5d8e6df0803e0a4ba43c4171c4f4286433cc3d
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2023-01-31 20:21:58 +1100

    updated material UI Basics

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
-import Box from "@mui/material/Box";
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
 import "./App.css";
-import { useState } from "react";
+import Muibutton from "./components/Muibutton";
+import MuiTypography from "./components/MuiTypography";
 
 function App() {
-  const [value, setValue] = useState(0);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-    console.log(event, value);
-  };
-
   return (
     <div className="App">
       <h1>Food villa</h1>
-      <Box sx={{ width: "100%", bgcolor: "background.paper" }}>
-        <Tabs value={value} onChange={handleChange} centered>
-          <Tab label="Home" />
-          <Tab label="" />
-          <Tab label="" />
-        </Tabs>
-      </Box>
+      {/* <MuiTypography /> */}
+      <Muibutton/>
     </div>
   );
 }
---
+++ src/components/Muibutton.js
@@ -0,0 +1,14 @@
+import { Stack, Button } from "@mui/material";
+import React from "react";
+
+const Muibutton = () => {
+  return (
+    <Stack spacing={2} direction="row">
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </Stack>
+  );
+};
+
+export default Muibutton;
