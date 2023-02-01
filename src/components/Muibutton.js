901e51e4ed3fc41695700646d776abbc725416e6
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2023-02-01 20:21:36 +1100

    update button and button group

--- src/components/Muibutton.js
+++ src/components/Muibutton.js
@@ -1,4 +1,5 @@
-import { Stack, Button } from "@mui/material";
+import { Stack, Button, IconButton, ButtonGroup } from "@mui/material";
+import DirectionsCarIcon from "@mui/icons-material/DirectionsCar";
 import React from "react";
 
 const Muibutton = () => {
@@ -30,6 +31,50 @@ const Muibutton = () => {
         <Button variant="contained" color="success">
           Success
         </Button>
+      </Stack>
+      <Stack display="block" spacing={2} direction="row">
+        <Button variant="contained" size="small">
+          Small
+        </Button>
+        <Button variant="contained" size="medium">
+          Medium
+        </Button>
+        <Button variant="contained" size="large">
+          Large
+        </Button>
+      </Stack>
+      <Stack spacing={2} direction="row">
+        <Button
+          variant="contained"
+          startIcon={<DirectionsCarIcon />}
+          disableRipple
+          onClick={() => alert("Clicked")}
+        >
+          car
+        </Button>
+        <Button
+          variant="contained"
+          endIcon={<DirectionsCarIcon />}
+          disableElevation
+        >
+          car
+        </Button>
+        <IconButton aria-label="car" color="success" size="small">
+          <DirectionsCarIcon />
+        </IconButton>
+      </Stack>
+      <Stack direction="row">
+        <ButtonGroup
+          variant="contained"
+          orientation="vertical"
+          size="small"
+          color="secondary"
+          aria-label="alignment button group"
+        >
+          <Button onClick={() => alert("left")}>Left</Button>
+          <Button onClick={() => alert("center")}>Center</Button>
+          <Button onClick={() => alert("right")}>Right</Button>
+        </ButtonGroup>
       </Stack>
     </Stack>
   );
