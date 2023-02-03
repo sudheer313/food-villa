ffdc24a2183894c08fa051d075223d5af63f81bc
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2023-02-04 09:29:31 +1100

    update styles to box component

--- src/components/MuiLayout.js
+++ src/components/MuiLayout.js
@@ -3,20 +3,32 @@ import React from "react";
 
 const MuiLayout = () => {
   return (
-    <Box
-      sx={{
-        backgroundColor: "primary.main",
-        color: "white",
-        height: "100px",
-        width: "100px",
-        padding: "16px",
-        "&:hover": {
-          backgroundColor: "primary.light",
-        },
-      }}
-    >
-      code evolution
-    </Box>
+    <>
+      <Box
+        sx={{
+          backgroundColor: "primary.main",
+          color: "white",
+          height: "100px",
+          width: "100px",
+          padding: "16px",
+          "&:hover": {
+            backgroundColor: "primary.light",
+          },
+        }}
+      >
+        code evolution
+      </Box>
+      <Box
+        display="flex"
+        height="100px"
+        width="100px"
+        bgcolor="success.light"
+        p={2}
+      >
+        {" "}
+        code evolution2
+      </Box>
+    </>
   );
 };
 
