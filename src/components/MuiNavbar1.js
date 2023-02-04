49ca1f67ea430e3f65cbd7c0845e58a096f26e26
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2023-02-04 15:57:28 +1100

    update menu list in navbar

--- src/components/MuiNavbar1.js
+++ src/components/MuiNavbar1.js
@@ -1,8 +1,26 @@
-import { AppBar, Button, IconButton, Toolbar, Typography } from "@mui/material";
-import { Box, Stack } from "@mui/system";
-import React from "react";
+import {
+  AppBar,
+  Button,
+  IconButton,
+  Toolbar,
+  Typography,
+  Menu,
+  MenuItem,
+  Box,
+  Stack,
+} from "@mui/material";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import { React, useState } from "react";
 
 const MuiNavbar1 = () => {
+  const [anchorE1, setAnchorE1] = useState("");
+  const open = Boolean(anchorE1);
+  const handleClick = (event) => {
+    setAnchorE1(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorE1(null);
+  };
   return (
     <AppBar position="static">
       <Toolbar>
@@ -21,15 +39,44 @@ const MuiNavbar1 = () => {
             />
           </a>
         </IconButton>
-        <Typography variant="h3" component="div" sx={{ flexGrow: "1" }}>
+        <Typography variant="h6" component="div" sx={{ flexGrow: "1" }}>
           Food Villa
         </Typography>
         <Stack direction="row" spacing={2}>
           <Button color="inherit">Features</Button>
           <Button color="inherit">Pricing</Button>
           <Button color="inherit">About</Button>
+          <Button
+            color="inherit"
+            id="resources-button"
+            onClick={handleClick}
+            aria-controls={open ? "resources-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            endIcon={<KeyboardArrowDownIcon />}
+          >
+            Resources
+          </Button>
           <Button color="inherit">Login</Button>
         </Stack>
+        <Menu
+          id="resources-menu"
+          anchorE1={anchorE1}
+          open={open}
+          MenuListProps={{ "aria-labelledby": "resource-button" }}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "right",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+        >
+          <MenuItem onClick={handleClose}>Blog</MenuItem>
+          <MenuItem onClick={handleClose}>Podcast</MenuItem>
+        </Menu>
       </Toolbar>
     </AppBar>
   );
